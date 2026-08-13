1e640817e0f7d191e17acc4ad51dd7aef1fcd2b2
Andrew <a1214443427@gmail.com>
2026-08-13 16:31:07 -0700

    Add form validation schema and form state typing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import Form from "./components/Form";
 
 export default function Home() {
-  return (
-    <div className="flex w-full h-full max-w-100 bg-[#F2FFFF]">
-      <Form />
-    </div>
-  );
+  const [phase, setPhase] = useState<"start" | "form" | "result">("start");
+  return <div className="flex w-full h-full max-w-100 bg-[#F2FFFF]">{<Form />}</div>;
 }
---
+++ app/type.ts
@@ -0,0 +1,60 @@
+import * as z from "zod";
+
+export const FormSchema = z
+  .object({
+    travelerCount: z.number().min(1).max(10),
+    from: z.string(),
+    to: z.string(),
+    startDate: z.iso
+      .date()
+      .refine(
+        (date) => date >= new Date().toLocaleDateString("en-CA"),
+        "Start date must be greater or equal to today",
+      ),
+    endDate: z.iso
+      .date()
+      .refine(
+        (date) => date >= new Date().toLocaleDateString("en-CA"),
+        "End date must be greater or equal to today",
+      ),
+    budget: z.number().min(0),
+  })
+  .refine((data) => data.endDate >= data.startDate, "End date must be greater than start date");
+
+export type FormData = z.infer<typeof FormSchema>;
+
+export const ResponseSchema = z.object({
+  startDate: z.string(),
+  endDate: z.string(),
+  startLocation: z.string(),
+  endLocation: z.string(),
+});
+
+export const ActivitiesSchema = z.object({
+  title: z.string(),
+  discretion: z.string(),
+});
+
+export class APIError extends Error {
+  code: number;
+  constructor(code: number, message: string) {
+    super();
+    this.code = code;
+    this.message = message;
+  }
+}
+
+export type FormState =
+  | {
+      phase: "initial";
+    }
+  | {
+      phase: "invalid";
+      error: z.ZodError;
+      prevData: FormData;
+    }
+  | {
+      phase: "error";
+      error: APIError;
+      prevData: FormData;
+    };
